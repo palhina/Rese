07cee2aa76e26862283ff7f3877112070946a26e
hitomi <pal96894089@gmail.com>
2024-04-09 12:47:49 +0900

    テストコード修正

--- src/tests/Feature/RatingControllerTest.php
+++ src/tests/Feature/RatingControllerTest.php
@@ -5,18 +5,40 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Shop;
 
 class RatingControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
+    // 評価ページ表示テスト
+    public function test_rate()
+    {
+        $user = User::factory()->create();
+        $shop = Shop::factory()->create();
+        $this->actingAs($user);
+        $response = $this->get("/rate/{$shop->id}");
         $response->assertStatus(200);
+        $response->assertViewIs('rating');
+    }
+
+    // 評価機能送信テスト
+    public function test_review()
+    {
+        $user = User::factory()->create();
+        $shop = Shop::factory()->create();
+        $this->actingAs($user);
+        $response = $this->post("/rate/{$shop->id}", [
+            'rating' => '5',
+            'comment' => 'testComment',
+        ]);
+        $this->assertDatabaseHas('ratings',[
+            'user_id' => $user->id,
+            'shop_id' => $shop->id,
+            'rating' => '5',
+            'comment' => 'testComment',
+        ]);
+        $response->assertViewIs('thanks_rate');
     }
 }
